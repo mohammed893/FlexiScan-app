5502f52ceb69a3d7024b7c51667b9e8383350042
mohammed893 <pspcpspc6@gmail.com>
2024-10-25 04:46:32 +0300

    [ADD] Flexi Controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flexiscan101/BluetoothScan/Bluetooth_Scan.dart';
+import 'package:flexiscan101/animation_module/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
+  
 }
 //hello world
 
@@ -23,7 +26,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  LoginScreen()//const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
