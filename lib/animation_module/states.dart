63edb7632be31e7501e277c0d141f1e937de07a3
mohammed893 <pspcpspc6@gmail.com>
2024-10-27 22:55:21 +0300

    cm

---
+++ lib/animation_module/states.dart
@@ -0,0 +1,9 @@
+abstract class AnimationStates {}
+
+class initState extends AnimationStates {}
+
+class StagingNewState extends AnimationStates {}
+
+class LoopState extends AnimationStates {}
+
+class IdleState extends AnimationStates{}
